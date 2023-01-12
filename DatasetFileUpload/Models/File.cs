8522f55541a0bcf3c3305caf6e61c60adad85679
Olof Olsson <olof.olsson@snd.gu.se>
2023-01-12 17:20:05 +0100

    add file and ro-crate helper

---
+++ DatasetFileUpload/Models/File.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace DatasetFileUpload.Models;
+
+public record File(
+    string Id,
+    long? ContentSize,
+    DateTime? DateCreated,
+    DateTime? DateModified,
+    string? EncodingFormat,
+    string? Sha256,
+    Uri? Url);
