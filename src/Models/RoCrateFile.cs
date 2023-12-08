8b2920e8641859af37d8b2a5cdfc27b1b8ec2815
Stefan Jakobsson <stefan.jakobsson@snd.gu.se>
2023-12-08 15:03:14 +0100

    RoCrateFile.ContentSize not nullable

--- src/Models/RoCrateFile.cs
+++ src/Models/RoCrateFile.cs
@@ -8,7 +8,7 @@ public class RoCrateFile
     [JsonPropertyName("@type")]
     public string RdfType => "File";
     public string Id { get; set; } = "";
-    public long? ContentSize { get; set; }
+    public long ContentSize { get; set; } = 0;
     public DateTime? DateCreated { get; set; }
     public DateTime? DateModified { get; set; }
     public string? EncodingFormat { get; set; }
