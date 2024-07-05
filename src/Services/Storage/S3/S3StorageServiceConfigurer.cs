a35b8d2d626f3688f16f2f4b70a05f6d10daa280
Stefan Jakobsson <stefan.jakobsson@snd.gu.se>
2024-07-05 16:06:01 +0200

    S3 / Add validation for MultiPart configuration values

--- src/Services/Storage/S3/S3StorageServiceConfigurer.cs
+++ src/Services/Storage/S3/S3StorageServiceConfigurer.cs
@@ -10,7 +10,13 @@ internal class S3StorageServiceConfigurer : IStorageServiceConfigurer<S3StorageS
     {
         services.AddOptionsWithValidateOnStart<S3StorageServiceConfiguration>()
            .Bind(configuration)
-           .ValidateDataAnnotations();
+           .ValidateDataAnnotations()
+           .Validate(c =>
+                c.MultiPartUploadThreshold < c.MultiPartUploadChunkSize * 10_000, 
+                nameof(S3StorageServiceConfiguration.MultiPartUploadChunkSize) +
+                " is too small to allow uploading larger objects than the value of " +
+                nameof(S3StorageServiceConfiguration.MultiPartUploadThreshold) +
+                "(max number of parts per upload is 10 000)");
 
         var s3Config = configuration.Get<S3StorageServiceConfiguration>()!;
 
