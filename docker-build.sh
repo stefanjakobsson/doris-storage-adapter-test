5b53b2dfdfe0c4b301cd8b618ab8c86898506d99
Stefan Jakobsson <stefan.jakobsson@snd.gu.se>
2025-10-21 13:06:16 +0200

    wip

--- docker-build.sh
+++ docker-build.sh
@@ -7,7 +7,7 @@ VERSION="$(bash resolve_version.sh)"
 echo "Resolved version: $VERSION"
 
 docker build \
-    --build-arg MINVERVERSIONOVERRIDE=$VERSION \
+    --build-arg VERSION=$VERSION \
     --build-arg CI=true \
     --build-arg SOURCE_DATE_EPOCH="$(git log -1 --pretty=%ct)" \
     .
