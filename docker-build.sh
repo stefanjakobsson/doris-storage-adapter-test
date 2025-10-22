9b63b4bcc69feb3beceb1b51fdf8d2a8979d9547
Stefan Jakobsson <stefan.jakobsson@snd.gu.se>
2025-10-22 16:58:00 +0200

    wip

--- docker-build.sh
+++ docker-build.sh
@@ -2,7 +2,7 @@
 
 docker build \
     -t doris-storage-adapter \
-    --build-arg VERSION="$(./resolve-version.sh)" \
+    --build-arg VERSION="$(./calculate-version.sh)" \
     --build-arg CI=true \
     --build-arg SOURCE_DATE_EPOCH="$(git log -1 --pretty=%ct)" \
     .
